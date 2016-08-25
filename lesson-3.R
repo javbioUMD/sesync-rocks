95c51709afdb8ce7d8f3911180b7c8f7c178b1d6
Javier Carpinteyro Ponce <Javier Carpinteyro Ponce>
2016-08-25 15:05:39 -0400

    Lesson 3 finished

--- lesson-3.R
+++ lesson-3.R
@@ -1,69 +1,50 @@
 ## Libraries and data
 
 library(dplyr)
-library(...)
-surveys <- read.csv(..., na.strings = "") %>%
+library(ggplot2)
+surveys <- read.csv("data/surveys.csv", na.strings = "") %>%
   filter(!is.na(species_id), !is.na(sex), !is.na(weight))
 
 ## Constructing layered graphics in ggplot
 
-ggplot(...,
-       ...) +
-  ...
-
 ggplot(data = surveys,
-       aes(x = species_id, y = weight)) +
-  ...
-  geom_point(...,
-             ...,
-             ...)
+       aes(x = species_id, y = weight)) + geom_point()
+  
+
+ggplot(data = surveys,aes(x = species_id, y = weight)) + 
+  geom_boxplot() + 
+  geom_point(stat = "summary", fun.y = "mean", color = "red")
+
+qplot(data = surveys, x = species_id, y = weight, geom_boxplot())
 
 ## Exercise 1
 
-...
+surveys_dm <- filter(surveys, species_id == "DM")
+year_wgt <- ggplot(data = ex1_1, aes(x = year, y = weight, color = sex)) + 
+  geom_point(stat = "summary", fun.y = "mean", size = 4) + geom_smooth(method = "lm")
 
 ## Adding and customizing scales
 
 surveys_dm <- filter(surveys, ...)
-ggplot(...,
-       aes(x = year, y = weight)) +
-  geom_point(...,
-             size = 3,
-             stat = "summary",
-             fun.y = "mean") +
-  ...
-
-year_wgt <- ggplot(data = surveys_dm,
-                   aes(...,
-                       ...,
-                       ...)) +
-  geom_point(aes(shape = sex),
-             size = 3,
-             stat = "summary",
-             fun.y = "mean") +
-  geom_smooth(method = "lm")
 year_wgt
 
 year_wgt <- year_wgt +
-  scale_color_manual(...,
-                     ...) +
-  ...(values = c(3, 2),
-      labels = c("Female", "Male"))
+  scale_color_manual(values = c("darkblue", "orange"), labels = c("Female", "Male"))
 year_wgt
 
 ## Exercise 2
 
-...
+ggplot(data = ex1_1, aes(weight, fill = sex)) + geom_histogram(binwidth = 1)
 
 ## Axes, labels and themes
 
 histo <- ggplot(data = surveys_dm,
                 aes(x = weight, fill = sex)) +
-  geom_...
+  geom_histogram(binwidth = 1)
 histo
 
 histo <- histo +
-  ...(title = "Dipodomys merriami weight distribution",
+  labs(title = "Dipodomys merriami weight distribution",
        x = "Weight (g)",
        y = "Count") +
   scale_x_continuous(limits = c(20, 60),
@@ -73,9 +54,9 @@ histo
 histo <- histo +
   theme_bw() +
   theme(legend.position = c(0.2, 0.5),
-        plot.title = ...,
-        ... = element_text(...),
-        ... = element_text(size = 13, vjust = 0))
+        plot.title = element_text(face = 'bold', vjust = 2),
+        axis.title.y = element_text(size = 13, vjust = 1),
+        axis.title.x = element_text(size = 13, vjust = 0))
 histo
 
 ## Facets
@@ -87,15 +68,16 @@ levels(surveys_dm$month) <- c("January", "February", "March", "April", "May", "J
 ggplot(data = surveys_dm,
        aes(x = weight)) +
   geom_histogram() +
-  ...
+  facet_wrap( ~ month) +
   labs(title = "DM weight distribution by month",
        x = "Count",
        y = "Weight (g)")
 
 ggplot(data = surveys_dm,
-       aes(x = weight, fill = month)) +
-  ...
+  aes(x = weight, fill = month)) +
+  geom_histogram()+
   facet_wrap( ~ month) +
+  geom_histogram(data = select(surveys_dm, -month), alpha = 0.2)+
   labs(title = "DM weight distribution by month",
        x = "Count",
        y = "Weight (g)") +
