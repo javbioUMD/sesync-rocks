e68c8c5ef573037c7f75f0c9891dd944d9001207
Javier Carpinteyro Ponce <Javier Carpinteyro Ponce>
2016-08-25 10:16:36 -0400

    learning R!

--- lesson-1.R
+++ lesson-1.R
@@ -1,16 +1,24 @@
 ## Scripting in the editor
 
-... <- c(...)
+vals <- c(5, 6, 12)
 
-... <- data.frame(...)
+# create a data frame
+data <- data.frame(counts = vals)
+
+# str to know the type of data
+str(data)
 
 ## Anatomy of a function
 
-function(...) {         # The arguments, or input, to the function goes here.  
-   ...                  # The body of the function goes here.
-   return(...)          # Any output of the function goes here.
+function(df) {         # The arguments, or input, to the function goes here.  
+   result <- df[[1, 1]]                  # The body of the function goes 
+   return(result)          # Any output of the function goes here.
 }
 
+first <- function(df) {         # The arguments, or input, to the function goes here.  
+  result <- df[[1, 1]]                  # The body of the function goes 
+  return(result)          # Any output of the function goes here.
+}
 ## Change the url for the origin repo
 
 system('git remote set-url origin ...')
